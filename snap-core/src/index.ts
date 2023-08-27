83788eb60c1ffdd237fa85b2497ac5a75e1faf77
Evan Pipta <3pipta@gmail.com>
2023-08-27 18:34:30 -0400

    snap: temp tx simulation testing

--- snap-core/src/index.ts
+++ snap-core/src/index.ts
@@ -1,14 +1,174 @@
 import "@metamask/snaps-types";
-import { Text, divider, heading, panel, text } from "@metamask/snaps-ui";
+import {
+  Divider,
+  Text,
+  divider,
+  heading,
+  panel,
+  text,
+} from "@metamask/snaps-ui";
 import {
   Keypair,
   SerializeConfig,
+  SimulatedTransactionResponse,
   Transaction,
   VersionedTransaction,
 } from "@solana/web3.js";
 import { deriveSolanaKeypair } from "./keypair";
 
+// const SOLANA_CLUSTER_URL = "https://api.mainnet-beta.solana.com";
+
+
+const SOLANA_CLUSTER_URL = "https://tx-sim.drift.trade";
+
 const DEBUG = false;
+
+export interface SimulationResultsRPCResponse {
+  jsonrpc: string;
+  result: {
+    context: {
+      slot: number;
+      apiVersion: string;
+    };
+    value?: SimulatedTransactionResponse;
+  };
+  id: number;
+}
+
+export const simulateTransaction = async (
+  serializedTx: Buffer,
+  addresses?: string[],
+  id = 1
+) => {
+  const secondaryParams = {
+    replaceRecentBlockhash: true,
+    encoding: "base64",
+  };
+
+  if (addresses) {
+    // @ts-ignore
+    secondaryParams.accounts = {
+      encoding: "jsonParsed",
+      addresses,
+    };
+  }
+
+  const body = JSON.stringify({
+    id,
+    jsonrpc: "2.0",
+    method: "simulateTransaction",
+    params: [serializedTx.toString("base64"), secondaryParams],
+  });
+
+  const res = await fetch(SOLANA_CLUSTER_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body
+  });
+
+  if (res.status >= 400) {
+    throw new Error(`Error - ${res.status} ${res.statusText}`);
+  }
+
+  let data: SimulationResultsRPCResponse;
+
+  try {
+    data = (await res.json()) as SimulationResultsRPCResponse;
+  } catch (err) {
+    const textdata = await res.text();
+    throw new Error(`${res.status} ${res.statusText} ${textdata}`);
+  }
+
+  return {
+    results: data?.result.value,
+    body
+  }
+};
+
+export const getSimulatedTransactionBalanceChanges = async (
+  simulationResults: SimulatedTransactionResponse,
+  addresses: string[],
+  walletAddress: string
+) => {
+  const accountsChanged: any[] = [];
+
+  // Also we need to know the current balance here, as well as the decimals
+  // In order to display human readable balance change
+  // So another rpc call for current balances?
+  simulationResults.accounts?.forEach((account, i) => {
+    if (
+      account?.owner === "11111111111111111111111111111111" &&
+      addresses[i] === walletAddress
+    ) {
+      accountsChanged.push({
+        symbol: "SOL",
+        lamports: account?.lamports,
+        address: addresses[i],
+      });
+    } else {
+      accountsChanged.push({
+        // data: account?.data,
+        mint: "",
+        symbol: "Unknown", // TODO how to get this?
+        lamports: account?.lamports,
+        address: addresses[i],
+      });
+    }
+  });
+
+  return accountsChanged.map((account) => {
+    return JSON.stringify(account);
+  });
+};
+
+const getSimulateTransactionDisplayText = async (
+  serializedTx: Buffer,
+  walletAddress: string,
+  addresses?: string[]
+) => {
+  const simulationResultsText = [];
+
+  try {
+    const {results: simulationResults, body} = await simulateTransaction(
+      serializedTx,
+      addresses
+    );
+    simulationResultsText.push(text(body))
+    if (simulationResults) {
+      // simulationResultsText.push(text(JSON.stringify(simulationResults)));
+      const balanceChanges = await getSimulatedTransactionBalanceChanges(
+        simulationResults,
+        addresses || [],
+        walletAddress
+      );
+      balanceChanges.forEach((balanceChange) => {
+        simulationResultsText.push(text(balanceChange));
+      });
+    } else {
+      simulationResultsText.push(text("Transaction simulation failed:"));
+      simulationResultsText.push(text("Unknown error"));
+      simulationResultsText.push(
+        text(
+          "You can still send this transaction, but estimated balance changes are unavailable"
+        )
+      );
+    }
+  } catch (err) {
+    simulationResultsText.push(text("Transaction simulation failed:"));
+    simulationResultsText.push(text(`${err}`));
+    simulationResultsText.push(
+      text(
+        "You can still send this transaction, but estimated balance changes are unavailable"
+      )
+    );
+  }
+
+  // simulationResultsText.push(text(serializedTx.toString("base64")));
+
+  return simulationResultsText;
+};
 
 export const showDebugDialog = (err: any) => {
   snap.request({
@@ -156,8 +316,22 @@ export const signTransactionHandler = async (params: SignTransactionParams) => {
     );
     const buf = Buffer.from(byteArray);
 
+    const keypair = (await deriveSolanaKeypair(params.accountIndex)) as Keypair;
+
+    const walletAddress = keypair.publicKey.toBase58();
+
     if (params.isVersionedTransaction) {
       const tx = VersionedTransaction.deserialize(buf);
+
+      const accountKeys = tx.message.getAccountKeys();
+      const addresses = accountKeys.staticAccountKeys.map((acct) =>
+        acct.toBase58()
+      );
+      const simulationResultsText = await getSimulateTransactionDisplayText(
+        buf,
+        walletAddress,
+        addresses
+      );
 
       const instructionDetailsText: Text[] = [];
 
@@ -183,6 +357,7 @@ export const signTransactionHandler = async (params: SignTransactionParams) => {
           type: "confirmation",
           content: panel([
             heading(`${params.origin} wants you to approve a transaction:`),
+            ...simulationResultsText,
             divider(),
             ...instructionDetailsText,
           ]),
@@ -190,10 +365,6 @@ export const signTransactionHandler = async (params: SignTransactionParams) => {
       });
 
       if (confirmed) {
-        const keypair = (await deriveSolanaKeypair(
-          params.accountIndex
-        )) as Keypair;
-
         tx.sign([keypair]);
 
         const signedTransaction = Buffer.from(tx.serialize());
@@ -206,6 +377,20 @@ export const signTransactionHandler = async (params: SignTransactionParams) => {
       }
     } else {
       const tx = Transaction.from(buf);
+
+      const addresses: Set<string> = new Set();
+
+      tx.instructions.forEach((ix) => {
+        ix.keys.forEach((key) => {
+          addresses.add(key.pubkey.toBase58());
+        });
+      });
+
+      const simulationResultsText = await getSimulateTransactionDisplayText(
+        buf,
+        walletAddress,
+        [...addresses]
+      );
 
       const instructionDetailsText: Text[] = [];
       tx.instructions.forEach((instruction, index) => {
@@ -226,14 +411,14 @@ export const signTransactionHandler = async (params: SignTransactionParams) => {
           content: panel([
             heading(`${params.origin} wants you to approve a transaction:`),
             divider(),
+            ...simulationResultsText,
+            divider(),
             ...instructionDetailsText,
           ]),
         },
       });
 
       if (confirmed) {
-        const keypair = (await deriveSolanaKeypair()) as Keypair;
-
         tx.sign(keypair);
 
         const signatures = tx.signatures;
