a094e889a3f0975e50a315886669bcc0230ec4f6
Evan Pipta <3pipta@gmail.com>
2023-09-08 16:08:00 -0400

    fix publish config for wallet adapter

--- snap-wallet-adapter/prebuild.ts
+++ snap-wallet-adapter/prebuild.ts
@@ -17,13 +17,12 @@ const readmeVersionRegex = /### Snap Version: (.*)/;
 const currentReadmeVersion = readmeFileContents.match(readmeVersionRegex)?.[1];
 
 if (currentReadmeVersion !== snapPackage.version) {
-  fs.writeFileSync(
-    readmePath,
-    readmeFileContents.replace(
-      readmeVersionRegex,
-      `### Smap Version: ${snapPackage.version}`
-    )
+  const newReadmeContents = readmeFileContents.replace(
+    /### Snap Version: .*/,
+    `### Snap Version: ${snapPackage.version}`
   );
+
+  fs.writeFileSync(readmePath, newReadmeContents);
 
   console.log(
     "\n*** Updated snap version in readme. Please commit and push these changes. ***\n"
